a199069560a24c01c2a9f0b1285eb131a91838c4
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-10-16 12:40:54 -0400

    complete

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Welcome to Band name Generator")
+
+city = input("Which city did you grew up in?\n")
+
+pet = input("What is the name of your pet?\n")
+
+bandName = city + " " + pet
+
+print("Your band name is: ", bandName)
