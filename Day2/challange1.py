cfef2ca1878759ac45c4bee8c7a3f4a23cff4fbd
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-10-26 11:43:02 -0400

    challange completed

---
+++ Day2/challange1.py
@@ -0,0 +1,7 @@
+inp = input(str("Enter the number: "))
+
+a = int(inp[0])
+b = int(inp[1])
+
+
+print(a + b)
--- Day2/type.py
+++ Day2/type.py
@@ -1,3 +1,7 @@
-num_char = len(input("What is your name?"))
-new_num_char = str(num_char)
-print(type(new_num_char))
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print(type(new_num_char))
+
+a = float(156)
+print(type(a))
+print(a)
