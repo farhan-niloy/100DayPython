160ba9fc8152598c1834757575f54f714e2c1645
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-18 10:34:30 -0500

    update

---
+++ Day4/day-4-1-exercise.py
@@ -0,0 +1,9 @@
+import random
+
+coin_flip = random.randint(0, 1)
+
+if coin_flip == 0:
+    print("Head")
+else:
+    print("Tell")
+
--- Day4/rand.py
+++ Day4/rand.py
@@ -5,7 +5,11 @@ random_integer = random.randint(1, 10)
 
 random_float = random.random()
 
+love_score = random.randint(1, 200)
+
 print(random_integer)
 print(random_float*30)
+print(f"Your love score is {love_score}")
 print(mm.pi)
+
 
