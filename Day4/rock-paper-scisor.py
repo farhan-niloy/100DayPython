1146d8d21637313a4c9d327b4ff2670257f3a003
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-19 06:36:48 -0500

    update

--- Day4/rock-paper-scisor.py
+++ Day4/rock-paper-scisor.py
@@ -33,26 +33,31 @@ usr = input("What do you choose? Type 1 for Rock, 2 for Paper or 3 for Scissors.
 
 int_usr = int(usr) - 1
 
-choose = moves[int_usr]
 
-computer = random.randint(0, 2)
+if int_usr >= 3 or int_usr < 0:
 
-computer_choose = moves[computer]
+    print("You typed an invalid number, you lose!")
 
-print(f"\n\nYou've chosen\n {choose}")
-print(f"Computer choose\n {computer_choose}")
-
-
-if int_usr == computer:
-    print("it's a tie")
-elif (int_usr + computer) == 1 and int_usr > computer:
-    print("you win!")
-elif (int_usr + computer) == 2 and int_usr > computer:
-    print("you win")
-elif (int_usr + computer) == 3 and int_usr > computer:
-    print("you win")
 else:
-    print("you loose")
+    choose = moves[int_usr]
+    print(f"\n\nYou've chosen\n {choose}")
+
+    computer = random.randint(0, 2)
+
+    computer_choose = moves[computer]
+
+    print(f"Computer choose\n {computer_choose}")
+
+    if int_usr == computer:
+        print("it's a tie")
+    elif (int_usr + computer) == 1 and int_usr > computer:
+        print("you win!")
+    elif (int_usr + computer) == 2 and int_usr > computer:
+        print("you win")
+    elif (int_usr + computer) == 3 and int_usr > computer:
+        print("you win")
+    else:
+        print("you loose")
 
 
 
