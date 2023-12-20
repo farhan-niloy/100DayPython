ec52e656c60622c8eb339763d5f4aa6de11817d2
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-20 12:38:16 -0500

    update

--- Day5/py-password-gen.py
+++ Day5/py-password-gen.py
@@ -9,8 +9,34 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
+
+ltr = []
+emoji = []
+num = []
+
+for l in range(0, nr_letters):
+    rand_letters = random.randint(0, len(letters))
+    ltr.append(letters[rand_letters])
+    l += 1
+print(ltr)
+
+for s in range(0, nr_symbols):
+    rand_symbols = random.randint(0, len(symbols))
+    emoji.append(symbols[rand_symbols])
+    s += 1
+print(emoji)
+
+for n in range(0, nr_numbers):
+    rand_numbers = random.randint(0, len(numbers))
+    num.append(numbers[rand_numbers])
+    n += 1
+print(num)
+
+
+
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
 #Hard Level - Order of characters randomised:
+
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
