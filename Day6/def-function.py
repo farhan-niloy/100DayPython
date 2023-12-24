3e1f099ab0549db586ec0e3acc1bd2b6b63b3324
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-24 11:10:55 -0500

    update

---
+++ Day6/def-function.py
@@ -0,0 +1,22 @@
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+def turn_right():
+    turn_around()
+    turn_left()
+
+
+move()
+move()
+move()
+turn_around()
+move()
+move()
+turn_right()
+move()
+move()
+turn_right()
+move()
