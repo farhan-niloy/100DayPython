1563009becfa9c23eedc96e3708ea1266dfced1a
Farhan Niloy <farhansarkerniloy@gmail.com>
2023-12-25 14:02:40 -0500

    update

--- Day7/Day-7-Hangman.py
+++ Day7/Day-7-Hangman.py
@@ -1,2 +1,13 @@
+import random
+
 insect_list = ["Ladybugs", "Cockroach", "Mantis"]
 
+guess = input("Guess a letter: ").lower()
+
+chosen_word = random.choice(insect_list)
+
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else:
+        print("wrong")
