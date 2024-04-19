57b31581269a63df45506eca780c04299a510a2e
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-19 23:11:34 +0600

    add day10

--- Day10/calculator/main.py
+++ Day10/calculator/main.py
@@ -1,1 +1,28 @@
-i
+from art import logo
+
+print(logo)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+dict = {}
+
+dict['+'] = "add"
+dict['-'] = "subtract"
+dict['*'] = "multiply"
+dict['/'] = "divide"
+
+print(dict)
