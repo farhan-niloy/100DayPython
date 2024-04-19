111371f486bd04b501062b9f6366607a1695cb9f
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-19 20:53:43 +0600

    add day9

---
+++ Day9/cities_visited.py
@@ -0,0 +1,18 @@
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "city_visitedMost": "Hamburg"},
+}
+
+travel_log2 = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 12
+    },
+
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": "5"
+    },
+]
