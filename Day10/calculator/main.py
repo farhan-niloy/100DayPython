f2230b55aca9fc094855dac25b6dd168cd4d0c07
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-19 23:46:17 +0600

    add day10

--- Day10/calculator/main.py
+++ Day10/calculator/main.py
@@ -2,26 +2,19 @@ from art import logo
 
 print(logo)
 
-
 def add(n1, n2):
     return n1 + n2
-
 
 def subtract(n1, n2):
     return n1 - n2
 
-
 def multiply(n1, n2):
     return n1 * n2
-
 
 def divide(n1, n2):
     return n1 / n2
 
-
-
 operations = {}
-
 operations['+'] = add
 operations['-'] = subtract
 operations['*'] = multiply
@@ -31,9 +24,21 @@ num1 = int(input("Enter first number: "))
 modeOperation = input("Choose an operation:  ")
 num2 = int(input("Enter second number: "))
 
+def perform_operation(operation, num1, num2):
+    for operator in operations:
+        if operation == operator:
+            operation_func = operations[operator]
+            calculation = operation_func(num1, num2)
+            return calculation
 
-for operator in operations:
-    if modeOperation == operator:
-        operation = operations[operator]
-        calculation = operation(num1, num2)
-        print(calculation)
+
+calculation = perform_operation(modeOperation, num1, num2)
+print(f"{num1} {modeOperation} {num2} = {calculation}")
+
+
+operation2 = input("Choose an operation:  ")
+num3 = int(input("Enter the next number: "))
+
+
+calculation2 = perform_operation(operation2, perform_operation(modeOperation, num1, num2), num3)
+print(f"{calculation} {operation2} {num3} = {calculation2}")
