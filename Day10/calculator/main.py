c70687f458fe8c20a1723c2b298e57485df211a9
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-20 00:07:35 +0600

    add day10

--- Day10/calculator/main.py
+++ Day10/calculator/main.py
@@ -24,21 +24,27 @@ num1 = int(input("Enter first number: "))
 modeOperation = input("Choose an operation:  ")
 num2 = int(input("Enter second number: "))
 
-def perform_operation(operation, num1, num2):
-    for operator in operations:
-        if operation == operator:
-            operation_func = operations[operator]
-            calculation = operation_func(num1, num2)
-            return calculation
+should_continue = True
+
+while should_continue:
+    def perform_operation(operation, num1, num2):
+        for operator in operations:
+            if operation == operator:
+                operation_func = operations[operator]
+                calculation = operation_func(num1, num2)
+                return calculation
 
 
-calculation = perform_operation(modeOperation, num1, num2)
-print(f"{num1} {modeOperation} {num2} = {calculation}")
+    calculation = perform_operation(modeOperation, num1, num2)
+    print(f"{num1} {modeOperation} {num2} = {calculation}")
 
+    continue_option = input("Type 'y' for further calculation: ")
+    if continue_option.lower() == 'y':
+        operation2 = input("Choose an operation:  ")
+        num3 = int(input("Enter the next number: "))
 
-operation2 = input("Choose an operation:  ")
-num3 = int(input("Enter the next number: "))
+        calculation2 = perform_operation(operation2, int(calculation), num3)
+        print(f"{calculation} {operation2} {num3} = {calculation2}")
 
-
-calculation2 = perform_operation(operation2, perform_operation(modeOperation, num1, num2), num3)
-print(f"{calculation} {operation2} {num3} = {calculation2}")
+    else:
+        should_continue = False
