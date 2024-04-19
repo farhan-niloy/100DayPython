bb3148f0d34c3acf3ec498cd846c4a249b6ea2e6
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-19 20:32:32 +0600

    add day9

--- Day9/dictionary.py
+++ Day9/dictionary.py
@@ -4,4 +4,15 @@ programming_dictionary = {
     "Star": "Wars",
 }
 
+empty_dictionary = {}
+
+empty_dictionary["1"] = "One"
+empty_dictionary["2"] = "Two"
+
+print(empty_dictionary)
+
 print(programming_dictionary["Fahmida"])
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
