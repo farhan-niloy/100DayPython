4a27c55e012f6a9a2451ee52407ef0fc38bc25dd
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-20 23:12:45 +0600

    add capestone

--- Day11/Capestone Project/main.py
+++ Day11/Capestone Project/main.py
@@ -1,4 +1,3 @@
-from art import logo
 import random
 import os
 
@@ -21,9 +20,25 @@ def calculate_scores(cards):
 
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
-        cards.append(11)
-
+        cards.append(1)  # Changed to add 1 instead of 11
     return sum(cards)
+
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw!"
+    elif computer_score == 0:
+        return "Lose! opponent has BlackJack"
+    elif user_score == 0:
+        return "Win with a BlackJack"
+    elif user_score > 21:
+        return "You went over 21! You lose"
+    elif computer_score > 21:
+        return "Opponent went over 21!, You win"
+    elif user_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose!"
 
 
 user_cards = []
@@ -34,20 +49,26 @@ for _ in range(2):
     user_cards.append(deal_cards())
     computer_cards.append(deal_cards())
 
-user_score = calculate_scores(user_cards)
-computer_score = calculate_scores(computer_cards)
-print(f"Your cards: {user_cards}, current score: {user_score}")
-print(f"Computer's first cards: {computer_cards[0]}")
+while not is_game_over:
+    user_score = calculate_scores(user_cards)
+    computer_score = calculate_scores(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first cards: {computer_cards[0]}")
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-    is_game_over = True
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")  # Corrected 'y' to 'y'
+        if user_should_deal.lower() == 'y':  # Changed 'y' to 'y'
+            user_cards.append(deal_cards())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_cards())
+    computer_score = calculate_scores(computer_cards)
 
 
-
-
-
-
-
-
-
-
+print(f" Your final hand: {user_cards}, final score: {user_score}")
+print(f" Computer final hand: {computer_cards}, final score: {computer_score}")
+print(compare(user_score, computer_score))
