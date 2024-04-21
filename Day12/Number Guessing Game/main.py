89a2fbcb2765919802a2bb412f7957ffbd76d47c
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-21 23:03:11 +0600

    Day12

--- Day12/Number Guessing Game/main.py
+++ Day12/Number Guessing Game/main.py
@@ -3,6 +3,7 @@ from random import randint
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
+
 def check_answer(guess, answer):
     if guess > answer:
         print("Too high!")
@@ -20,13 +21,17 @@ def set_difficulty():
         return HARD_LEVEL_TURNS
 
 
-print("Welcome to Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
+def game():
+    print("Welcome to Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    turns = set_difficulty()
+    print(f"You have {turns} guesses left.")
 
-turns = set_difficulty()
-print(f"You have {turns} guesses left.")
-guess = int(input("Make a guess: "))
+    guess = 0
+    while guess != answer:
+        guess = int(input("Make a guess: "))
+        check_answer(guess, answer)
 
 
-
+game()
