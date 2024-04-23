851ef0c3d416b4cf4567e342e14bb363aa56a01b
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-04-24 00:55:30 +0600

    addd cofeemachine

--- Day15/Cofee Machine Project/main.py
+++ Day15/Cofee Machine Project/main.py
@@ -40,13 +40,35 @@ def get_report():
     print(f"Money: {profit}")
 
 
-def is_resources_sufficient(ordered_ingredients):
-
+def is_resources_sufficient(ordered_ingredients, order):
+    """Returns True when order can be made, otherwise returns False."""
     for item in ordered_ingredients:
         if ordered_ingredients[item] >= resources[item]:
-            print("Sorry there's not enough water!")
+            print(f"Sorry there's not enough {item} for {order}!")
+            return False
+        return True
+
+def process_coins():
+    """Returns the total calculated from the coins inserted"""
+    print("Please insert coins: ")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
 
 
+def is_transaction_successful(money, cost):
+    """Returns True when the payment is accepted, or False if money is insufficient"""
+    if money >= cost:
+        change = round(money - cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += cost
+        return True
+    else:
+        print("Sorry there's not enough money!")
+        return False
 
 
 is_on = True
@@ -59,5 +81,7 @@ while is_on:
         get_report()
     else:
         drink = MENU[choice]
-        is_resources_sufficient(drink["ingredients"])
+        if is_resources_sufficient(drink["ingredients"], choice):
+            payment = process_coins()
+            is_transaction_successful(payment, drink["cost"])
 
