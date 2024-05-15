d1854f64efa12c7814dcb5e9d6e95f7cbf9b686a
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-05-15 20:37:44 +0600

    Day 19

--- Day19/main.py
+++ Day19/main.py
@@ -23,6 +23,13 @@ if user_bet:
 while is_race_on:
 
     for turtle in all_turtle:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print("You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
 
