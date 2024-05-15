efac722a67f5897105d62890ca7eae24bc113de6
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-05-15 20:22:06 +0600

    Day 19

--- Day19/main.py
+++ Day19/main.py
@@ -2,8 +2,16 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.setup(500*3, 400*3)
+screen.setup(500*2, 400*2)
 user_bet = screen.textinput("Make your bet", prompt="Which turtle will win the race?")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+y_position = [-70, -40, -10, 20, 50, 80]
+
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x = -230*2, y = y_position[turtle_index]*2)
 
 screen.exitonclick()
