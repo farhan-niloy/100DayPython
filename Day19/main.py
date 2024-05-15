d86f5ae61901d929e157a9be286ca1321b4b34d8
Farhan Niloy <farhansarkerniloy@gmail.com>
2024-05-15 19:54:10 +0600

    Day 19

--- Day19/main.py
+++ Day19/main.py
@@ -6,6 +6,21 @@ screen = Screen()
 def move_forward():
     tim.forward(10);
 
+def move_backward():
+    tim.backward(10);
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(move_forward, "w")
+screen.onkey(move_forward, "s")
+screen.onkey(move_forward, "a")
+screen.onkey(move_forward, "d")
+
 screen.exitonclick()
